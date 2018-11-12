bce6a89c5d55c65cea9a74671a5326d31ac9743b
freeza354 <freeza354@gmail.com>
2018-11-12 20:16:53 +0700

    Update Game Mechanics AI Follow Waypoints

---
+++ Assets/Script/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    [SerializeField]
+    private float EnemySpeeed = 10f;
+
+    private Transform target;
+    private int WavepointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        target = LoadWaypoint.Waypoints[0];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 direction = target.position - transform.position;
+
+        transform.Translate(direction.normalized * EnemySpeeed * Time.deltaTime, Space.World);
+
+	}
+}
---
+++ Assets/Script/LoadWaypoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadWaypoint : MonoBehaviour {
+
+    public static Transform[] Waypoints;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        Waypoints = new Transform[transform.childCount];
+
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            Waypoints[i] = transform.GetChild(i);
+        }
+
+    }
+
+}
