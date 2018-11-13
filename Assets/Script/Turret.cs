3a4d45db44d11289a012cef2222f16699e91c0fc
Reza Faiz <freeza354@gmail.com>
2018-11-14 00:14:32 +0700

    Fix turret movement

--- Assets/Script/Turret.cs
+++ Assets/Script/Turret.cs
@@ -3,17 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Turret : MonoBehaviour {
+    
+    public static Transform target;
 
     [SerializeField]
-    private Transform target;
+    private float range = 15f;
+
+    private string EnemyTag = "Enemy";
+    private float TurnSpeed = 2f;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        dir.Normalize();
+
+        float rotZ = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        Quaternion LookRotate = Quaternion.Euler(new Vector3(0f, 0f, -(rotZ + 90)));
+        Vector3 Rotate = Quaternion.Lerp(transform.rotation, LookRotate, TurnSpeed * Time.deltaTime).eulerAngles;
+        transform.rotation = Quaternion.Euler(Rotate);
+
 	}
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(EnemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
 }
