25fcd712b905e6f0b72ce2396a757cd7f5db02b1
Reza Faiz <freeza354@gmail.com>
2018-11-24 21:42:11 +0700

    Update Spawner

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,9 +10,10 @@ public class GameManager : MonoBehaviour {
     public float SpawnCooldownInterval = 5f;
     public float WaveTimerInterval;
     public int WaveCounter = 0;
+    public int EnemySpawnIndex = 5;
 
     [Header("Target Enemy Settings")]
-
+    public float HealthOrgan;
 
     [SerializeField]
     private GameObject enemyPrefab;
@@ -20,6 +21,8 @@ public class GameManager : MonoBehaviour {
     private Transform SpawnPoint;
     [SerializeField]
     private float timeDelaySpawner = 5f;
+
+    public static float HealthOrganPublic;
 
     private float WaveIntervalPrivate;
     private float cooldown = 2f;
@@ -29,6 +32,7 @@ public class GameManager : MonoBehaviour {
 	void Start () {
 
         WaveIntervalPrivate = WaveTimerInterval;
+        HealthOrganPublic = HealthOrgan;
 
 	}
 	
@@ -46,8 +50,8 @@ public class GameManager : MonoBehaviour {
 
     IEnumerator SpawnWave()
     {
-        enemySpawnIndex++;
-        for (int i = 0; i < enemySpawnIndex; i++)
+        EnemySpawnIndex = EnemySpawnIndex + ((WaveCounter + 1) * 2);
+        for (int i = 0; i < EnemySpawnIndex; i++)
         {
             SpawnEnemy();
             yield return new WaitForSeconds(0.5f);
@@ -57,7 +61,7 @@ public class GameManager : MonoBehaviour {
 
     void SpawnEnemy()
     {
-        Instantiate(enemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+        Instantiate(EnemyPrefab, new Vector3(SpawnPosition.transform.position.x, Random.Range(-8f, 8f), 0), SpawnPosition.transform.rotation);
     }
 
 }
